ab5d4c007b7e44c7b86da496d2848ed73951efcf
semseimarton <semsei.marton@gmail.com>
2024-03-06 12:10:00 +0100

    2_kerdes

--- ZH1.sql
+++ ZH1.sql
@@ -1,3 +1,24 @@
 SELECT count(*) AS 'Feltételeknek megfelelő fpglalások száma'
 FROM Foglalas
 WHERE YEAR(METTOL)=YEAR(MEDDIG) AND MONTH(METTOL)<>MONTH(MEDDIG)
+
+
+
+
+
+
+
+SELECT szh.SZALLAS_ID, szh.SZALLAS_NEV, 
+
+              COUNT(*) AS 'Klímás szobák száma'
+ 
+FROM Szallashely szh JOIN Szoba sz ON szh.SZALLAS_ID=sz.SZALLAS_FK
+
+WHERE sz.klimas='i'
+
+GROUP BY szh.SZALLAS_ID, szh.SZALLAS_NEV
+
+
+
+
+
