5e121cded454e60c1e478a60fcd0c0ea413fdc7a
semseimarton <semsei.marton@gmail.com>
2024-03-06 12:21:23 +0100

    3_kerdes

--- ZH1.sql
+++ ZH1.sql
@@ -22,3 +22,12 @@ GROUP BY szh.SZALLAS_ID, szh.SZALLAS_NEV
 
 
 
+SELECT  SZOBA_ID,
+        YEAR(METTOL) AS 'Foglalás éve',
+        COUNT(*) AS 'Foglalások száma'
+FROM Szoba sz JOIN Foglalas f ON sz.SZOBA_ID=f.SZOBA_FK
+WHERE SZOBA_ID%2=1
+GROUP BY SZOBA_ID, YEAR(METTOL)
+HAVING COUNT(*)>10
+
+
